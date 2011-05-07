3254e1c1a2afb3dc834fca51cb9697867e68cd1a
co <coer@kth.se>
2011-05-07 11:02:28 +0200

    Linking works now!

--- linkit.sh
+++ linkit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#TODO fix this shit.
 #Warning! Running this script will overwrite any conflicting dotfiles in
 #your home directory.
 
@@ -7,12 +8,13 @@
 for file in $(find -maxdepth 1 -mindepth 1 -type f -name '.*' -printf "%f\n")
 do
     printf "%-30s ->                    %-30s\n" "./$file" "$HOME/$file"
-    ln -f -n "./$file" "$HOME/$file"
+    ln -f -n -t $HOME "$(pwd)/$file"
 done
 
 #create symbolic links for directories.
-for file in $(find -maxdepth 1 -mindepth 1 -type d -name '.*' -printf "%f\n")
+for dir in $(find -maxdepth 1 -mindepth 1 -type d -name '.*' -printf "%f\n")
 do
-    printf "%-30s ->                    %-30s\n" "./$file" "$HOME/$file"
-    ln -s -f -n "./$file" "$HOME/$file"
+    printf "%-30s ->                    %-30s\n" "$(pwd)/$dir" "$HOME/$dir"
+    ln -n -s "$(pwd)/$dir" "$HOME/$dir" 2> /dev/null ||
+        rm -f -r "$HOME/$dir" && ln -n -v -s "$(pwd)/$dir" "$HOME/$dir"
 done
