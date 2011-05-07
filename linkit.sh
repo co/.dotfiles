dbd5d8a21467989fae2637aa98a79b14fa2d9995
co <coer@kth.se>
2011-05-07 11:03:28 +0200

    Now with correct verbose output.

--- linkit.sh
+++ linkit.sh
@@ -7,14 +7,12 @@
 #create hard links for files.
 for file in $(find -maxdepth 1 -mindepth 1 -type f -name '.*' -printf "%f\n")
 do
-    printf "%-30s ->                    %-30s\n" "./$file" "$HOME/$file"
-    ln -f -n -t $HOME "$(pwd)/$file"
+    ln -v -f -n -t $HOME "$(pwd)/$file"
 done
 
 #create symbolic links for directories.
 for dir in $(find -maxdepth 1 -mindepth 1 -type d -name '.*' -printf "%f\n")
 do
-    printf "%-30s ->                    %-30s\n" "$(pwd)/$dir" "$HOME/$dir"
-    ln -n -s "$(pwd)/$dir" "$HOME/$dir" 2> /dev/null ||
+    ln -v -n -s "$(pwd)/$dir" "$HOME/$dir" 2> /dev/null ||
         rm -f -r "$HOME/$dir" && ln -n -v -s "$(pwd)/$dir" "$HOME/$dir"
 done
